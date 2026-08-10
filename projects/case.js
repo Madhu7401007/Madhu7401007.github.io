7e85dd530018caf8c888375edaccc1a878b00c34
Madhusudhan007 <vmadhusudhan7401@gmail.com>
2026-08-10 20:48:53 +0100

    Create case.js

---
+++ projects/case.js
@@ -0,0 +1,4 @@
+var io = new IntersectionObserver(function(entries){
+entries.forEach(function(e){ if(e.isIntersecting){ e.target.classList.add('in'); io.unobserve(e.target); } });
+},{threshold:0.1});
+document.querySelectorAll('.reveal').forEach(function(el){ io.observe(el); });
